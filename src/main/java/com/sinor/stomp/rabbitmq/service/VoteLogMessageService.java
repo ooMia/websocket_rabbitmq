69d456dd9d7631c7f8c1bcd78beef8d1eac86a2d
Hyeon-hak Kim <07ily@naver.com>
2024-01-03 22:22:56 +0900

    🐛 Fix : rollback queryDSL

--- src/main/java/com/sinor/stomp/rabbitmq/service/VoteLogMessageService.java
+++ src/main/java/com/sinor/stomp/rabbitmq/service/VoteLogMessageService.java
@@ -1,11 +1,7 @@
 package com.sinor.stomp.rabbitmq.service;
 
-import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sinor.stomp.vote.model.dto.response.VoteLogResponseDto;
-import com.sinor.stomp.vote.model.entity.board.vote.QVote;
-import com.sinor.stomp.vote.model.entity.board.vote.VoteItem;
 import com.sinor.stomp.vote.repository.VoteItemRepository;
-import jakarta.persistence.EntityManager;
 import java.util.NoSuchElementException;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,14 +12,11 @@ public class VoteLogMessageService {
 
     private final RabbitTemplate rabbitTemplate;
     private final VoteItemRepository voteItemRepository;
-    private final EntityManager entityManager;
 
     @Autowired
-    public VoteLogMessageService(RabbitTemplate rabbitTemplate, VoteItemRepository voteItemRepository,
-                                 EntityManager entityManager) {
+    public VoteLogMessageService(RabbitTemplate rabbitTemplate, VoteItemRepository voteItemRepository) {
         this.rabbitTemplate = rabbitTemplate;
         this.voteItemRepository = voteItemRepository;
-        this.entityManager = entityManager;
     }
 
     public void broadcastLogByItemId(VoteLogResponseDto voteLogResponseDto) {
@@ -34,14 +27,6 @@ public class VoteLogMessageService {
 
 
     private Long findVoteIdFromVoteItemId(Long voteItemId) throws NoSuchElementException {
-
-        // Find voteLogEntity from voteLogId
-        VoteItem voteItem = voteItemRepository.findById(voteItemId).orElseThrow();
-
-        // Find voteId from voteItemEntity
-        QVote qVote = QVote.vote;
-        return new JPAQueryFactory(entityManager)
-                .selectFrom(qVote)
-                .where(qVote.items.contains(voteItem)).fetchFirst().getId();
+        return voteItemRepository.findById(voteItemId).orElseThrow().getVoteId();
     }
 }
